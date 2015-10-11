931eee5d9207a0f262dcef60c343b1796bda7b78
Wladimir Kröker <wladimirkroeker@googlemail.com>
2015-10-11 17:50:16 +0200

    Basic movement and Point Effector Test

---
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public Transform playerTransform;
+
+    private float followSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+    void FixedUpdate()
+    {
+        float x = Mathf.Lerp(transform.position.x, playerTransform.position.x, Time.deltaTime * followSpeed);
+        float y = Mathf.Lerp(transform.position.y, playerTransform.position.y, Time.deltaTime * followSpeed);
+
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Control/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public Rigidbody2D playerBody;
+    public Transform groundCheckTransform;
+
+    private float moveSpeed = 3.0f;
+    private float jumpForce = 300.0f;
+
+    private bool grounded = true;
+    private bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        grounded = Physics2D.Linecast(transform.position, groundCheckTransform.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (grounded && Input.GetButtonDown("Jump"))
+            jump = true;
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        playerBody.velocity = new Vector2(horizontalAxis * moveSpeed, playerBody.velocity.y);
+
+        if (jump)
+        {
+            playerBody.AddForce(new Vector2(0.0f, jumpForce));
+            jump = false;
+        } 
+    }
+}
